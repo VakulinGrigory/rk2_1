64909a77d5c536137bdcceb99778d33a1ac4a023
VakulinGrigory <github.vakulingd@mail.ru>
2024-06-06 09:02:06 +0300

    Update BusinessMediator.cpp

--- BusinessMediator.cpp
+++ BusinessMediator.cpp
@@ -1,32 +1,33 @@
 #include "BusinessMediator.h"
 #include "EstateOwner.h"
 #include "GroceryStore.h"
-#include "Restaraunt.h"
+#include "Restaurant.h"
 
-BusinessMediator::BusinessMediator(EstateOwner& estateOwner, GroceryStore& groceryStore, Restaraunt& restaraunt)
-    : estateOwner_(estateOwner), groceryStore_(groceryStore), restaraunt_(restaraunt) {
+BusinessMediator::BusinessMediator(EstateOwner& estateOwner, GroceryStore& groceryStore, Restaurant& restaurant)
+    : estateOwner_(estateOwner), groceryStore_(groceryStore), restaurant_(restaurant) {
     estateOwner_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
     groceryStore_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
-    restaraunt_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
+    restaurant_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
 }
 
 void BusinessMediator::EstateRentPriceChanged(std::int32_t oldPrice, std::int32_t newPrice) {
     groceryStore_.AlterPrice((newPrice - oldPrice) / 10000);
-    restaraunt_.AlterPrice((newPrice - oldPrice) / 1000);
+    restaurant_.AlterPrice((newPrice - oldPrice) / 1000);
 }
 
 void BusinessMediator::GroceryStockChanged(std::int32_t currentStock) {
     if (currentStock > 0) {
-        restaraunt_.SetIsOpened(design::AccessKey<BusinessMediator>(), true);
+        restaurant_.SetIsOpened(design::AccessKey<BusinessMediator>(), true);
     } else {
-        restaraunt_.SetIsOpened(design::AccessKey<BusinessMediator>(), false);
+        restaurant_.SetIsOpened(design::AccessKey<BusinessMediator>(), false);
     }
 }
 
 void BusinessMediator::GroceryPriceChanged(std::int32_t oldPrice, std::int32_t newPrice) {
-    restaraunt_.AlterPrice(newPrice - oldPrice);
+    restaurant_.AlterPrice(newPrice - oldPrice);
 }
 
 void BusinessMediator::FoodIsCooked() {
     groceryStore_.Sell();
 }
+
