e8d080e3d61bfeeedb65b1f84f257a96316f2233
VakulinGrigory <github.vakulingd@mail.ru>
2024-06-06 08:57:14 +0300

    updated BusinessMediator.cpp

--- BusinessMediator.cpp
+++ BusinessMediator.cpp
@@ -1,30 +1,30 @@
 #include "BusinessMediator.h"
 #include "EstateOwner.h"
 #include "GroceryStore.h"
-#include "Restaurant.h"
+#include "Restaraunt.h"
 
-BusinessMediator::BusinessMediator(EstateOwner& estateOwner, GroceryStore& groceryStore, Restaurant& restaurant)
-    : estateOwner_(estateOwner), groceryStore_(groceryStore), restaurant_(restaurant) {
+BusinessMediator::BusinessMediator(EstateOwner& estateOwner, GroceryStore& groceryStore, Restaraunt& restaraunt)
+    : estateOwner_(estateOwner), groceryStore_(groceryStore), restaraunt_(restaraunt) {
     estateOwner_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
     groceryStore_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
-    restaurant_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
+    restaraunt_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
 }
 
 void BusinessMediator::EstateRentPriceChanged(std::int32_t oldPrice, std::int32_t newPrice) {
     groceryStore_.AlterPrice((newPrice - oldPrice) / 10000);
-    restaurant_.AlterPrice((newPrice - oldPrice) / 1000);
+    restaraunt_.AlterPrice((newPrice - oldPrice) / 1000);
 }
 
 void BusinessMediator::GroceryStockChanged(std::int32_t currentStock) {
     if (currentStock > 0) {
-        restaurant_.SetIsOpened(design::AccessKey<BusinessMediator>(), true);
+        restaraunt_.SetIsOpened(design::AccessKey<BusinessMediator>(), true);
     } else {
-        restaurant_.SetIsOpened(design::AccessKey<BusinessMediator>(), false);
+        restaraunt_.SetIsOpened(design::AccessKey<BusinessMediator>(), false);
     }
 }
 
 void BusinessMediator::GroceryPriceChanged(std::int32_t oldPrice, std::int32_t newPrice) {
-    restaurant_.AlterPrice(newPrice - oldPrice);
+    restaraunt_.AlterPrice(newPrice - oldPrice);
 }
 
 void BusinessMediator::FoodIsCooked() {
