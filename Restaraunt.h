4cc6de6d103adcb55056dba73985fa21a22d515e
VakulinGrigory <github.vakulingd@mail.ru>
2024-06-06 08:49:21 +0300

    created Restaraunt.h

---
+++ Restaraunt.h
@@ -0,0 +1,19 @@
+#ifndef RESTAURANT_H
+#define RESTAURANT_H
+
+#include "BusinessMediator.h"
+
+class Restaurant {
+public:
+    std::int32_t CookFood();
+    std::int32_t AlterPrice(std::int32_t priceChange);
+    BusinessMediator* SetBusinessMediator(design::AccessKey<BusinessMediator>, BusinessMediator* mediator);
+    void SetIsOpened(design::AccessKey<BusinessMediator>, bool isOpened);
+
+private:
+    BusinessMediator* mediator_{ nullptr };
+    bool isOpened_{ true };
+    std::int32_t price_{ 500 };
+};
+
+#endif // RESTAURANT_H
